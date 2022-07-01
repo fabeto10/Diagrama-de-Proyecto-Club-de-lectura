d78d2c2a4cb886fa8ffc8d4a23a36679bbb662c8
fabeto10 <f0414475@gmail.com>
2022-07-01 20:19:06 +0000

    listo

--- src/models.py
+++ src/models.py
@@ -7,6 +7,7 @@ from sqlalchemy import create_engine
 from eralchemy import render_er
 
 Base = declarative_base()
+# El ejemplo del diagrama antes de hacerlo se encuentra aqui: mi_diagrama_ejemplo.png
 
 class User(Base):
     __tablename__ = 'user'
@@ -14,38 +15,62 @@ class User(Base):
     name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
     password = Column(String(250), nullable=False)
-    favorite = relationship("Favorite", back_populates="parent")
+    home = relationship("home", back_populates="parent")
 
-class Favorite(Base):
-    __tablename__ = 'favorite'
+class Profile(Base):
+    __tablename__= "profile"
+    id = Column(String(250), primary_key=True)
+    user = relationship("user", back_populates="children")
+    user_id = Column(Integer, ForeignKey("user.id"))
+
+class Followers(Base):
+    __tablename__="followers"
+    id = Column(String(250), primary_key=True)
+    user = relationship("user", back_populates="children")
+    number = Column(String(250), nullable=False)
+    user_from_id = Column(Integer, ForeignKey("user.id"))
+    user_to_id = Column(Integer, ForeignKey("user.id"))
+
+class Following(Base):
+    __tablename__="following"
+    id = Column(String(250), primary_key=True)
+    user = relationship("user", back_populates="children")
+    number = Column(String(250), nullable=False)
+    user_from_id = Column(Integer, ForeignKey("user.id"))
+    user_to_id = Column(Integer, ForeignKey("user.id"))
+
+class Direct_message(Base):
+    __tablename__="direct_message"
+    id = Column(String(250), primary_key=True)
+    text = Column(String(250), nullable=False)
+    user_from_id = Column(Integer, ForeignKey("user.id"))
+    user_to_id = Column(Integer, ForeignKey("user.id"))
+
+class Home(Base):
+    __tablename__ = 'home'
     id = Column(Integer, primary_key=True)
     user = relationship("user", back_populates="children")
     user_id = Column(Integer, ForeignKey("user.id"))
-    user = relationship("favorite", back_populates="children")
-    planet = Column(String(250), nullable=False)
-    character = Column(String(250), nullable=False)
-    # character_id = Column(Integer, ForeignKey(".id"))
-    # character = relationship("favorite", back_populates="children")
+    user_name=Column(String(250), nullable=False)
+    email=Column(String(250), nullable=False)
+    password=Column(String(250), nullable=False)
 
-class Planet(Base):
-    __tablename__ = 'planet'
+class Media(Base):
+    __tablename__ = 'media'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    rotation_period = Column(String(250), nullable=False)
-    population = Column(String(250), nullable=False)
-    terrain = Column(String(250), nullable=False)
-    favorite_id = Column(Integer, ForeignKey("favorite.id"))
-    favorite = relationship("favorite", back_populates="children")
+    url_image = Column(String(250), nullable=False)
+    url_video = Column(String(250), nullable=False)
+    home_id = Column(Integer, ForeignKey("home.id"))
+    home = relationship("home", back_populates="children")
 
-class Character(Base):
-    __tablename__ = 'character'
+class Comment(Base):
+    __tablename__ = 'comment'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    height = Column(String(250), nullable=False)
-    hair_color = Column(String(250), nullable= False)
-    gender = Column(String(250), nullable=False)
-    favorite_id = Column(Integer, ForeignKey("favorite.id"))
-    favorite = relationship("favorite", back_populates="children")
+    comment_text = Column(String(250), nullable=False)
+    home_id = Column(Integer, ForeignKey("home.id"))
+    home = relationship("home", back_populates="children")
+    autor_id = Column(Integer, ForeignKey("user.id"))
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
