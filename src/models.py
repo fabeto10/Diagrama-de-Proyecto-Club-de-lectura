d3ee9e55dca25238d5c01a520e9ccd9f7582e1e0
fabeto10 <f0414475@gmail.com>
2022-08-04 19:13:45 +0000

    se agrega db de proyecto Club de lectura

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -10,65 +10,43 @@ Base = declarative_base()
 # El ejemplo del diagrama antes de hacerlo se encuentra aqui: mi_diagrama_ejemplo.png
 
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__="user"
     id = Column(Integer, primary_key=True)
+    user_name = Column(String(120), nullable=False)
+    phone_number = Column(String(80), nullable=False)
+    email = Column(String(120), nullable=False)
+    password = Column(String(80), nullable=False)
+    Address = Column(String(150), nullable=False)
+    is_active = Column(Boolean(), nullable=False)
+    comment = relationship("Comment", back_populates="user")
+    favorite_books = relationship("FavoriteBooks", back_populates="user")
+
+class FavoriteBooks(Base):
+    __tablename__ = 'favorite_books'
+    id = Column(Integer, primary_key=True)
+    user = relationship("User", back_populates="favorite_books")
+    user_id = Column(Integer, ForeignKey("user.id"))
+    book_id = Column(Integer, ForeignKey("book.id"))
+    book = relationship("Book", back_populates="favorite_books")
+
+class Book(Base):
+    __tablename__ = 'book'
+    id = Column(Integer, primary_key=True)
+    rating = Column(String(250), nullable=False)
     name = Column(String(250), nullable=False)
-    email = Column(String(250), nullable=False)
-    password = Column(String(250), nullable=False)
-    home = relationship("home", back_populates="parent")
-
-class Profile(Base):
-    __tablename__= "profile"
-    id = Column(String(250), primary_key=True)
-    user = relationship("user", back_populates="children")
-    user_id = Column(Integer, ForeignKey("user.id"))
-
-class Followers(Base):
-    __tablename__="followers"
-    id = Column(String(250), primary_key=True)
-    user = relationship("user", back_populates="children")
-    number = Column(String(250), nullable=False)
-    user_from_id = Column(Integer, ForeignKey("user.id"))
-    user_to_id = Column(Integer, ForeignKey("user.id"))
-
-class Following(Base):
-    __tablename__="following"
-    id = Column(String(250), primary_key=True)
-    user = relationship("user", back_populates="children")
-    number = Column(String(250), nullable=False)
-    user_from_id = Column(Integer, ForeignKey("user.id"))
-    user_to_id = Column(Integer, ForeignKey("user.id"))
-
-class Direct_message(Base):
-    __tablename__="direct_message"
-    id = Column(String(250), primary_key=True)
-    text = Column(String(250), nullable=False)
-    user_from_id = Column(Integer, ForeignKey("user.id"))
-    user_to_id = Column(Integer, ForeignKey("user.id"))
-
-class Home(Base):
-    __tablename__ = 'home'
-    id = Column(Integer, primary_key=True)
-    user = relationship("user", back_populates="children")
-    user_id = Column(Integer, ForeignKey("user.id"))
-    user_name=Column(String(250), nullable=False)
-
-class Media(Base):
-    __tablename__ = 'media'
-    id = Column(Integer, primary_key=True)
-    url_image = Column(String(250), nullable=False)
-    url_video = Column(String(250), nullable=False)
-    home_id = Column(Integer, ForeignKey("home.id"))
-    home = relationship("home", back_populates="children")
+    authors = Column(String(250), nullable=False)
+    cover = Column(String(250), nullable=False)
+    year = Column(String(250), nullable=False)
+    favorite_books = relationship("FavoriteBooks", back_populates="book")
 
 class Comment(Base):
     __tablename__ = 'comment'
     id = Column(Integer, primary_key=True)
-    comment_text = Column(String(250), nullable=False)
-    home_id = Column(Integer, ForeignKey("home.id"))
-    home = relationship("home", back_populates="children")
-    autor_id = Column(Integer, ForeignKey("user.id"))
-
+    user = relationship("User", back_populates="comment")
+    user_id = Column(Integer, ForeignKey("user.id"))
+    user_name = Column(String(120), nullable=False)
+    book_id = Column(Integer, ForeignKey("book.id"))
+    content = Column(String(250), nullable=False)
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
